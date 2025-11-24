115fa2fbbca067dc0056c96102da3c76f2195c18
EnderSlayer121 <owenwiddup@gmail.com>
2025-11-23 19:25:29 -0500

    Player looks around properly

--- player.gd
+++ player.gd
@@ -11,4 +11,19 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed * delta)
 		velocity.y = move_toward(velocity.y, 0, speed * delta)
 	
+	if velocity != Vector2.ZERO:
+		play_movement_animation(velocity)
+	
 	move_and_slide()
+
+func play_movement_animation(velocity: Vector2):
+	#left v right
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("right")
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("left")
+	#up v down
+	if velocity.y > 0:
+		$AnimatedSprite2D.play("front")
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play("back")
