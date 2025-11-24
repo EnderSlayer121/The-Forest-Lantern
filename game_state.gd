67a2629ed57c379b93b569098938f0dd2d1364e8
EnderSlayer121 <owenwiddup@gmail.com>
2025-11-23 19:57:04 -0500

    Working Fire Crystals

---
+++ game_state.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var num_crystals = 0
+var lives = 3
+signal crystal_change(new_crystals)
+signal life_change(new_lives)
+enum PlayerState {BASE, MASKED, SWORDED}
+var current_state = PlayerState.BASE
+
+func give_crystal(num: int) -> void:
+	num_crystals += num
+	emit_signal("crystal_change", num_crystals)
+
+func life_lost() -> void:
+	lives -= 1
+	emit_signal("life_change", lives)
+	if lives <= 0:
+		get_tree().change_scene_to_file("res://level/fail.tscn")
+	else:
+		get_tree().reload_current_scene()
